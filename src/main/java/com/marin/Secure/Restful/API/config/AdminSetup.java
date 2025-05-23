9ea2062e3d6a5a0840c9894fa9dfa7887e1f5845
Luis Angel Marin <la.marin@outlook.com>
2025-05-23 16:24:24 -0700

    Admin Setup

--- src/main/java/com/marin/Secure/Restful/API/config/AdminSetup.java
+++ src/main/java/com/marin/Secure/Restful/API/config/AdminSetup.java
@@ -5,6 +5,7 @@ import com.marin.Secure.Restful.API.entities.User;
 import com.marin.Secure.Restful.API.repository.RoleRepository;
 import com.marin.Secure.Restful.API.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -19,24 +20,33 @@ import java.util.List;
 @Component
 public class AdminSetup implements CommandLineRunner {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final RoleRepository roleRepository;
+
+    private final String adminUser;
+
+    private final String adminPass;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private RoleRepository roleRepository;
-
+    public AdminSetup(UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository roleRepository, @Value("${auth.admin.username}") String adminUser ,@Value("${auth.admin.password}") String adminPass) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
+        this.adminUser = adminUser;
+        this.adminPass = adminPass;
+    }
 
     @Override
     public void run(String... args) throws Exception {
 
         // Check if the Admin user already exists.
-        if (userRepository.findByUsername("admin").isEmpty()) {
+        if (userRepository.findByUsername(adminUser).isEmpty()) {
             User admin = new User();
-            admin.setUsername("admin");
-            admin.setPassword(passwordEncoder.encode("password")); // Hash password
+            admin.setUsername(adminUser);
+            admin.setPassword(passwordEncoder.encode(adminPass)); // Hash password
 
             Role role = roleRepository.findByName("ADMIN").orElseThrow();
 
